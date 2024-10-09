176622c58209c7934c2b1cc7bdda04d23192fb08
Fernandohka <fernando2005andrade@gmail.com>
2024-10-09 17:15:26 -0300

    segunda-rota

--- src/app/primeira-rota/page.tsx
+++ src/app/primeira-rota/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
 interface IData {
     id: string,
@@ -28,24 +28,28 @@ const PrimeiraRota = () => {
     }, []);
 
     return (
-        <div className="flex flex-wrap justify-center p-2 gap-5">
-            {characters.map((item) => {
-                return(
-                    <div key={item.id} className="w-[300px] border border-black rounded-md flex flex-col items-center p-2">
-                        <h2>{item.name}</h2>
-                        <div>
-                            <Image src={item.image} alt="character image" height={10000} width={10000} className="w-auto h-[300px]" priority={true}></Image>
-                        </div>
-                        <p className="text-justify">Ki: {item.ki}</p>
-                        <p className="text-justify">Ki Maximo: {item.maxKi}</p>
-                        <p className="text-justify">Raça: {item.race}</p>
-                        <p className="text-justify">Genero: {item.gender}</p>
-                        <p className="text-justify">Descrição: {item.description}</p>
-                        <p className="text-justify">Afiliação: {item.affiliation}</p>
-                    </div>
-                )
-            })}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex flex-wrap justify-center p-2 gap-5">
+                <div className="flex flex-wrap justify-center p-2 gap-5w-full lg:w-3/4">
+                    {characters.map((item) => {
+                        return(
+                            <div key={item.id} className="w-[350px] border border-black rounded-md flex flex-col items-center p-2 gap-2">
+                                <h2 className="text-3xl"><b>{item.name}</b></h2>
+                                <div>
+                                    <Image src={item.image} alt="character image" height={10000} width={10000} className="w-auto h-[300px]" priority={true}></Image>
+                                </div>
+                                <p className="text-justify"><b>Ki:</b> {item.ki}</p>
+                                <p className="text-justify"><b>Ki Maximo:</b> {item.maxKi}</p>
+                                <p className="text-justify"><b>Raça:</b> {item.race}</p>
+                                <p className="text-justify"><b>Genero:</b> {item.gender}</p>
+                                <p className="text-justify"><b>Descrição:</b> {item.description}</p>
+                                <p className="text-justify"><b>Afiliação:</b> {item.affiliation}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </Suspense>
     )
 }
 
--- src/app/segunda-rota/page.tsx
+++ src/app/segunda-rota/page.tsx
@@ -1,10 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState, Suspense } from "react";
+
+import { api } from "../constants/api";
+
+interface IDados {
+  id: string,
+  name: string,
+  ki: string,
+  maxKi: string,
+  race: string,
+  gender: string,
+  description: string,
+  image: string,
+  affiliation: string,
+}
 
 const SegundaRota = () => {
+    const [pagina, setPagina] = useState<string | undefined>("");
+    const [nome, setNome] = useState<string | undefined>("");
+    const [dados, setDados] = useState<IDados[]>([]);
+    const [erroMsg, setErroMsg] = useState<string>("");
+
+    useEffect(() => {
+      setErroMsg("");
+      if (nome != undefined && nome != "") {
+        api.get(`/characters?name=${nome}`).then((res) => {
+          if (dados[0] == undefined) {
+            setErroMsg("Personagem não encontrado.");
+          }
+          setDados(res.data);
+        })
+      }
+      else if (pagina != undefined && pagina != "") {
+        api.get(`/characters?page=${pagina}`).then((res) => {
+          if (dados[0] == undefined) {
+            setErroMsg("Página não encontrada.");
+          }
+          setDados(res.data.items);
+        })
+      }
+      else{
+        api.get(`/characters`).then((res) => {
+          if (dados[0] == undefined) {
+            setErroMsg("Erro.");
+          }
+          setDados(res.data.items);
+        })
+      }
     
+      
+    }, [pagina, nome])
 
     return (
         <>
-            <h1>segunda rota</h1>
+            <div className="flex justify-center flex-wrap gap-2 p-1">
+              <input type="text" className="border border-black rounded gap-2 p-1" placeholder="Nome" value={nome} onChange={(e) => {setNome(e.target.value)}} />
+              <input type="text" className="border border-black rounded gap-2 p-1" placeholder="Página" value={pagina} onChange={(e) => {setPagina(e.target.value)}} />
+            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+              <div className="flex flex-wrap justify-center p-2 gap-5">
+                {dados[0] == undefined && <p>{erroMsg}</p>}
+                {dados.map((item) => {
+                  return (
+                    <div key={item.id} className="w-[300px] border border-black rounded-md flex flex-col items-center p-2">
+                      <h2>{item.name}</h2>
+                      <div>
+                        <Image src={item.image} alt="character image" height={10000} width={10000} className="w-auto h-[300px]" priority={true}></Image>
+                      </div>
+                      <p className="text-justify">Ki: {item.ki}</p>
+                      <p className="text-justify">Ki Maximo: {item.maxKi}</p>
+                      <p className="text-justify">Raça: {item.race}</p>
+                      <p className="text-justify">Genero: {item.gender}</p>
+                      <p className="text-justify">Descrição: {item.description}</p>
+                      <p className="text-justify">Afiliação: {item.affiliation}</p>
+                    </div>
+                  )
+                })}
+              </div>
+            </Suspense>
         </>
     )
 }
